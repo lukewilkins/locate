8565b7a48c5209c785e3aeb33f164ee1ff558ec1
Gary Saunders <gary@powrit.com>
2013-05-02 19:53:16 -0400

    Adding option to trust proxy data.

--- config/options.php
+++ config/options.php
@@ -4,7 +4,7 @@ return array(
 
 	// Number of minutes before location data should be refreshed
 	// Set to "0" to not automatically refresh
-	'refresh_rate' => 0,
+	'refresh_rate' => 1,
 
 	// MaxMind API Key (http://www.maxmind.com/app/web_services#city)
 	'maxmind_key' => '',
@@ -15,14 +15,20 @@ return array(
 	// Service priority
 	// Options: 'MaxMind', 'IPInfoDB', 'FreeGeoIP'
 	'service_priority' => array(
+        'FreeGeoIP',
 		'IPInfoDB',
-		'MaxMind',
-		'FreeGeoIP',
+		//'MaxMind',
 	),
 
 	// Fallback IP Address
 	// Allows you to use a fallback IP address when the user's IP
 	// is 127.0.0.1 or cannot be determined
-	'fallback_ip' => '64.90.182.55',
+	'fallback_ip' => '108.168.252.214',
+	
+    // Trust Proxy Data
+    // When running in the cloud behind a load balancer, enable this
+    // option to get the proper end user IP via the "HTTP_X_FORWARDED_FOR"
+    // header rather than the "REMOTE_ADDR" server var
+    'trustproxydata'    => true,
 
 );
